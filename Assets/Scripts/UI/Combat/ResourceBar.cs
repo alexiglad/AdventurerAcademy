3cbd8d2ca873523b24335dfaec0667e838e66052
Cedric Johnson <Cedricmjohnson2020@gmail.com>
2021-08-29 14:21:28 -0400

    WIP

---
+++ Assets/Scripts/UI/Combat/ResourceBar.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceBar : MonoBehaviour
+{
+    [SerializeField] FloatValueSO lerpTimer;
+    [SerializeField] FloatValueSO chipTimer;
+}
---
+++ Assets/Scripts/UI/Combat/ResourceBarManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceBarManager : MonoBehaviour
+{
+    [SerializeField] List<ResourceBarUI> bars;
+
+    void OnEnable()
+    {
+        List<ResourceBarUI>  bars = GetComponentsInChildren<ResourceBarUI>().ToList();
+    }
+
+    public void UpdateHealth()
+    {
+
+    }
+}
