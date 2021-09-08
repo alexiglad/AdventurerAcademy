98fc8255d786f82f38bfe37304e7c3cdc649cd24
Cedric Johnson <Cedricmjohnson2020@gmail.com>
2021-09-08 17:56:26 -0400

    UIHandler

--- Assets/Scripts/UI/UIHandler.cs
+++ Assets/Scripts/UI/UIHandler.cs
@@ -112,6 +112,7 @@ public class UIHandler : ScriptableObject
     {
         if (doubleM)
         {
+            Debug.Log("here");
             doubleMovement.SetActive(true);
         }
         else
