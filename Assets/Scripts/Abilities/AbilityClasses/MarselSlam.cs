12130e2d3301f9b362efadd9f42830d051b80454
alexiglad <alexigladstone@gmail.com>
2021-09-30 13:31:38 -0400

    TestPush

---
+++ Assets/Scripts/Abilities/AbilityClasses/MarselSlam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// 
+/// 
+/// 
+/// 
+/// </summary>
+[CreateAssetMenu(menuName = "ScriptableObjects/Ability/MarselSlam")]
+public class MarselSlam : Ability
+{
+    void OnEnable()
+    {
+        damage = 5;
+        range = 5;
+    }
+
+
+    public override void HandleAbility(Character attacker, Character attackee, Ability ability)
+    {
+        abilityProcessor.Damage(attackee, damage);
+
+    }
+}
